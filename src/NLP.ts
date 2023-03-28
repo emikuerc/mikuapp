a84a6c2cc9d74eaa260316cf2e829fa6966120c4
emikuerc <129227251+emikuerc@users.noreply.github.com>
2023-03-28 14:25:04 -0700

    Update NLP.ts

--- src/NLP.ts
+++ src/NLP.ts
@@ -2,8 +2,6 @@ import { containerBootstrap } from '@nlpjs/core';
 import { Nlp } from '@nlpjs/nlp';
 import { LangEn } from '@nlpjs/lang-en';
 import { LangId } from '@nlpjs/lang-id';
-import model from './nlp/model.json';
-
 const container = await containerBootstrap();
 
 container.use(Nlp);
@@ -13,6 +11,5 @@ container.use(LangId);
 const NLP = container.get('nlp');
 
 NLP.settings.autoSave = false;
-NLP.import(model);
 
 export default NLP;
